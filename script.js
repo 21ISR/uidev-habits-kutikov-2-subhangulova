35d515f3ef595e02502b0cf0c84156114cc1bc15
Ksu <akialwayscomeback@gmail.com>
2026-05-25 05:45:13 +0000

    oi

--- script.js
+++ script.js
@@ -2,10 +2,37 @@ const formEl = document.querySelector("form.form")
 const inputEl = document.querySelector(".form__input")
 const habitContainerEl = document.querySelector(".habit-list")
 
+let habbits = []
+
+function renderHabit(habit) {
+    return `
+    <li class="habit">
+        <div class="habit__header">
+            <h2 class="habit__name">${habit.name}</h2>
+            <button class="habit__delete"></button>
+        </div>
+        <div class="habit__days"></div>
+    </li>
+    `
+}
+
+function render(){
+    const habitsEl = habits.map(habit => renderHabit(habit)).join("")
+    habitContainerEl.innerHTML = habitsEl
+}
+
 function handleSubmit(e) {
     e.preventDefault()
-    habitContainerEl.innerHTML += inputEl.value
 
+    const newHabit = {
+        name:inputEl.value,
+        id: Date.now(),
+    }
+
+    habits.push(newHabit)
+
+    render()
+    
     inputEl.value = ""
 }
 
